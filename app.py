7ec9cb9d135ec268639ad6335acca97174cd7897
saradakv <66003677+Sarada-K@users.noreply.github.com>
2023-03-27 17:23:30 +0000

    added css

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-  return "Hello World"
+  return render_template("dashboard.html")
+
+
+@app.route("/contactus")
+def contactus():
+  return render_template("contact-us.html")
+
+
+@app.route("/aboutus")
+def aboutus():
+  return render_template("about-us.html")
 
 
 if __name__ == '__main__':
