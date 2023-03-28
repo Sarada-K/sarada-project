46ed9370fc3a40ad4d093000cfe6c84dc39675b8
saradakv <66003677+Sarada-K@users.noreply.github.com>
2023-03-28 07:39:15 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+BOOKS = [{
+  "id": 1,
+  "title": "1984",
+  "author": "George Orwell",
+  "price": "Rs. 100",
+  "rating": 4.9
+}, {
+  "id": 2,
+  "title": "The Lord of the Rings",
+  "author": "R.R. Tolkien",
+  "price": "Rs. 2300",
+  "rating": 4.9
+}, {
+  "id": 3,
+  "title": "The Kite Runner",
+  "author": "Khaled Hosseini",
+  "price": "Rs. 357",
+  "rating": 4.7
+}, {
+  "id": 4,
+  "title": "Harry Potter and the Philosopher’s Stone",
+  "author": "J.K. Rowling",
+  "price": "Rs. 420",
+  "rating": 5
+}, {
+  "id": 5,
+  "title": "Slaughterhouse-Five",
+  "author": "Kurt Vonnegut",
+  "price": "Rs. 524",
+  "rating": 4.6
+}]
 
 
 @app.route("/")
 def index():
-  return render_template("dashboard.html")
+  return render_template("dashboard.html", books=BOOKS)
 
 
 @app.route("/contactus")
